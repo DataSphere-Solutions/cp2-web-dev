85e80fdc463a894ae0c1d5450623a6a2abfaa9fd
Ian <ianmonteiromoreira@gmail.com>
2024-05-01 20:01:30 -0300

    Página de login estilizada - Commit por Ian

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,14 @@
 // SlideShow Automático
 let imagens=['./src/assets/img/bike1.png','./src/assets/img/bike2.jpg','/src/assets/img/bike3.jpg', '/src/assets/img/bike4.jpg'];
 let index = 0;
-let time= 3000;
+let time = 3000;
 
 function slideShow(){
     document.getElementById('imgBanner').src=imagens[index];
     index++;
 
     if(index == imagens.length){
-        index=0;
+        index = 0;
     }
     setTimeout('slideShow()', time);
 }
