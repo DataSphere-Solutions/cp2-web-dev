fee1efdbd6d891413cda59f1f234518d18d7204e
Ian <ianmonteiromoreira@gmail.com>
2024-05-01 17:49:49 -0300

    S

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 window.alert('Seja bem-vindo')
+
+// SlideShow Automático
+let imagens=['./src/assets/img/bike1.png','./src/assets/img/bike2.jpg','/src/assets/img/bike3.jpg', '/src/assets/img/bike4.jpg'];
+let index = 0;
+let time= 3000;
+
+function slideShow(){
+    document.getElementById('imgBanner').src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout('slideShow()', time);
+}
+slideShow();
