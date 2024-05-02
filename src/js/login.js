8ab70b1de20da79c4ee34bec659f32dca9623ef1
Ian <ianmonteiromoreira@gmail.com>
2024-05-01 21:06:47 -0300

    Página contato finalizada - Commit por Ian

--- src/js/login.js
+++ src/js/login.js
@@ -9,3 +9,15 @@ function validar() {
         window.alert('Usuário ou senha inválidos! Preencha os dados e tente novamente.')
     }
 }
+
+function formulario() {
+    let nome = document.querySelector('#nomehtml').value;
+    let telefone = document.querySelector('#telhtml').value;
+    let email = document.querySelector('#emailhtml').value;
+
+    if (nome == '' || telefone == '' || email == '') {
+        window.alert('Erro! Algum campo não foi preenchido. Verifique os dados e tente novamente.')
+    } else {
+        window.alert('Dados enviados com sucesso!')
+    }
+}
