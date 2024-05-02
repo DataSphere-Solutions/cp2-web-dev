2014fbda51e0e43ca7fe83526974b7a1d99b5d43
Ian <ianmonteiromoreira@gmail.com>
2024-05-01 22:55:42 -0300

    Finalizando com sucesso o quiz - Commit por Ian

---
+++ src/js/perguntas.js
@@ -0,0 +1,89 @@
+export default [
+    {
+        question: 'Qual é a finalidade principal do desviador de uma bicicleta?',
+        answers: [
+            { option: 'Para controlar os freios', correct: false },
+            { option: 'Para mudar a corrente entre as marchas', correct: true },
+            { option: 'Fornecer suporte para o suporte de bicicletas', correct: false },
+            { option: 'Para alimentar as luzes da bicicleta', correct: false },
+        ],
+    },
+    {
+        question: 'Qual destas NÃO é um tipo de bicicleta?',
+        answers: [
+            { option: 'Bicicleta de estrada', correct: false },
+            { option: 'Bicicleta de montanha', correct: false },
+            { option: 'Bicicleta híbrida', correct: false },
+            { option: 'Bicicleta submarina', correct: true },
+        ],
+    },
+    {
+        question: 'Qual o nome da peça que liga o guidão à roda dianteira?',
+        answers: [
+            { option: 'O garfo', correct: true },
+            { option: 'O caule', correct: false },
+            { option: 'O fone de ouvido', correct: false },
+            { option: 'O escora', correct: false },
+        ],
+    },
+    {
+        question: 'Quantos raios tem uma roda de bicicleta típica?',
+        answers: [
+            { option: '12', correct: false },
+            { option: '24', correct: false },
+            { option: '36', correct: false },
+            { option: 'Varia, mas geralmente entre 24 e 36', correct: true },
+        ],
+    },
+    {
+        question: 'Qual é o termo para o quadro principal de uma bicicleta, ao qual o espigão, o guidão e os pedais são fixados?',
+        answers: [
+            { option: 'O chassi', correct: false },
+            { option: 'O esqueleto', correct: false },
+            { option: 'A moldura', correct: true },
+            { option: 'O corpo', correct: false },
+        ],
+    },
+    {
+        question: 'Qual material é mais comumente usado na fabricação de quadros de bicicletas de estrada de última geração?',
+        answers: [
+            { option: 'Aço', correct: false },
+            { option: 'Alumínio', correct: false },
+            { option: 'Fibra de carbono', correct: true },
+            { option: 'Titânio', correct: false },
+        ],
+    },
+    {
+        question: 'Qual é o melhor colocado no ranking brasileiro de ciclismo?',
+        answers: [
+            { option: 'Rafael Andriato', correct: true },
+            { option: 'Murilo Affonso', correct: false },
+            { option: 'Rafael Reis', correct: false },
+        ],
+    },
+    {
+        question: 'Qual é o atual campeão de ciclismo estilo livre nas olimpiadas de Tóquio?',
+        answers: [
+            { option: 'Daniel Dhers', correct: false },
+            { option: 'Logan Martin', correct: true },
+            { option: 'Declan Brooks', correct: false },
+        ],
+    },
+    {
+        question: 'Onde foi a primeira cidade que teve uma competição de ciclismo?',
+        answers: [
+            { option: 'Lisboa', correct: false },
+            { option: 'Washington DC', correct: false },
+            { option: 'Paris', correct: true },
+        ],
+    },
+    {
+        question: 'Qual é o pais com mais medalhas olimpicas no ciclismo?',
+        answers: [
+            { option: 'Italia', correct: true },
+            { option: 'Países Baixos', correct: false },
+            { option: 'Estados Unidos', correct: false },
+            { option: 'Brasil', correct: false },
+        ],
+    },
+]
