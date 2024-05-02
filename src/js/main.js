a05b4381a5b6f08859d09ffb72b28ec950814532
Ian <ianmonteiromoreira@gmail.com>
2024-05-01 23:20:04 -0300

    cores.js removido, juntado tudo em main.js - Commit por Ian

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
+window.alert('Seja bem-vindo')
+
+// SlideShow Automático
+let imagens=['./src/assets/img/bike1.png','./src/assets/img/bike2.jpg','./src/assets/img/bike3.jpg', './src/assets/img/bike4.jpg'];
+let index = 0;
+let time = 3000;
+
+function slideShow(){
+    document.getElementById('imgBanner').src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index = 0;
+    }
+    setTimeout('slideShow()', time);
+}
+slideShow();
+
 // Função que alterna entre 3 cores de fundo do index
 function mudarCor() {
     function getRandomInt(max) {
